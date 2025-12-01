25fb9e0e2e2d772fdcfae0656fa39afff3f845b8
lcalastro <calastro@gmail.com>
2025-11-30 22:13:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const SECRET = 'agsus-secret-2025'; // Em prod: use env var
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/vendor/adminlte', express.static(path.join(__dirname, 'node_modules/admin-lte/dist')));
+
 
 // Inicializa banco com tabelas novas
 db.exec(`
