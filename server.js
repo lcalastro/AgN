6d20b6842a1babae0b0697e2f5830ab45da50253
lcalastro <calastro@gmail.com>
2025-12-04 12:18:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Database = require('better-sqlite3');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 const fs = require('fs');
